0143bc394bc2134a2813bf96385a54c91b16881c
ganjinlei <404032765@qq.com>
2016-06-29 16:20:21 +0800

    629相对路径

--- wap/js/config.js
+++ wap/js/config.js
@@ -13,33 +13,12 @@ var IOSSiteUrl = "http://" + parHost + "/app.ipa"; //"http://v3.haoid.cn/app.ipa
 var AndroidSiteUrl = "http://" + parHost + "/app.apk"; //"http://v3.haoid.cn/app.apk";
 var WeiXinOauth = false;
 
-if (nowHost == mbHost) {
-//	changHost=parHost
-//	var getElementsObj=function(attrname, TagName) {
-//		var classobj = new Array(); //定义数组 
-//		var classint = 0; //定义数组的下标 
-//		if (typeof(TagName) == 'undefined') {
-//			var tags = document.getElementsByTagName("*"); //获取HTML的所有标签 
-//		}else{
-//			
-//			var tags = document.getElementsByTagName(TagName); //获取HTML的所有标签 
-//		}
-//		alert(tags.length)
-//		for (var i in tags) { //对标签进行遍历 
-//			if (tags[i].nodeType == 1) {
-//				if (tags[i].getAttribute(attrname) != null) {
-//						classobj[classint] = tags[i];
-//						console.log(classobj[classint])
-//						classint++;
-//				};
-//			};
-//		};
-//		return classobj; //返回组成的数组 
-//	};
-//
-//	var imgUrl=function(){
-//		var a = getElementsObj('src', 'img');
-//		console.log(a.length)
-//	}
-//	imgUrl();
+function changHostReturn(){
+	return changHost
+}
+function filterUrl(Furl){
+	if(Furl.indexOf('/')==0){
+		Furl='http://'+parHost+Furl;
+	}
+	return Furl;
 }
