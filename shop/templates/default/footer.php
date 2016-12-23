2bb27abcee740ee82caa2170cb6469a03179d56d
unknown <>
2016-12-23 23:47:05 +0800

    百度统计

--- shop/templates/default/footer.php
+++ shop/templates/default/footer.php
@@ -1,5 +1,19 @@
 <?php defined('InShopNC') or exit('Access Invalid!');?> 
 <?php //echo getChat($layout);?>
+	<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
 <div id="faq">
   <div class="faq-wrapper">
     <?php if(is_array($output['article_list']) && !empty($output['article_list'])){ ?><ul>
